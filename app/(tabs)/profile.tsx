d40baca6d030a0da33446bbd1e941ba70efc8932
Walter Wagude <walterwagude@Walters-MacBook-Pro.local>
2025-02-28 16:05:05 -0500

    updated styles name for profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { View, Text } from "react-native";
 import { user } from "../mockData/user"; // Import the user data
 import ProfileInfo from "../components/ProfileInfo";
-import styles from "../../styles/ProfileStyles"; // Import the styles
+import profileStyles from "../../styles/ProfileStyles"; // Import the styles
 
 const ProfilePage = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>User Profile</Text>
-      <Text style={styles.subtitle}>Manage your account settings here.</Text>
+    <View style={profileStyles.container}>
+      <Text style={profileStyles.title}>User Profile</Text>
+      <Text style={profileStyles.subtitle}>Manage your account settings here.</Text>
       <ProfileInfo user={user} /> {/* Pass user as a prop */}
     </View>
   );
